418b9c7f46d3f530b109524dd610a34e56a3d906
Eddie Chen <livehighvu06@gmail.com>
2023-01-25 21:03:19 +0800

    feat 新增nav

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { BsFillMoonStarsFill } from "react-icons/bs";
+
 function App() {
   return (
-    <main>
-      <h1 className="text-3xl font-bold underline">TEST</h1>
+    <main className="px-10">
+      <section className="min-h-screen">
+        <nav className="py-10 mb-12 flex justify-between">
+          <h1 className="text-xl font-bold font-burtons">Chen Yi Xian</h1>
+          <ul className="flex items-center gap-10">
+            <li>
+              <BsFillMoonStarsFill className="cursor-pointer text-2xl" />
+            </li>
+            <li>
+              <a
+                href="#"
+                className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white font-bold px-4 py-2 rounded-md"
+              >
+                Resume
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </section>
     </main>
   );
 }
