9becb6cc4cfa1c409e9774e06fc0aafefbc85106
Eddie Chen <livehighvu06@gmail.com>
2024-07-09 12:27:55 +0800

    Refactor about section to use data mapping for content display.

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import Header from "./components/Header";
 import Section from "./components/Section";
 import Grid from "./components/Grid";
 import GridItem from "./components/GridItem";
-import WKX from "./components/WKX";
+import BioItem from "./components/BioIem";
 import Container from "./components/Container";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 
 // Import bio list data
 import { bioList } from "./data/data";
+import { about } from "./data/data";
 import { skill } from "./data/data";
 import { portfolio } from "./data/data";
 
@@ -26,21 +27,22 @@ const App = () => (
           custom="dark:bg-slate-800 dark:rounded-md dark:border dark:border-slate-700"
           title="About"
         >
-          <p className="py-2 leading-8 text-gray-800 dark:text-white">
-            <span className="block font-bold">【動機】</span>
-            過去因家庭因素暫別職場，而家庭事務告一段落後，2019年因朋友介紹而開始接觸網頁設計，後來發現自己對前端技術充滿熱情，因此選擇轉向前端職業方向。在轉職的過程中，我遇到了各種挑戰，但我堅持不懈，透過自主學習和思考，逐步提升自己的技能和能力。最終在2020年成功轉職成為前端工程師。
-          </p>
-          <p className="py-2 leading-8 text-gray-800 dark:text-white">
-            <span className="block font-bold">【轉職過後】</span>
-            而轉職過後迎面而來的是各種挑戰，客戶對於網頁的嚴格要求，讓我在每次製作完成後不斷檢視自己，利用業外之餘不斷學習，研究新技術外，也不斷回頭溫習，期許能成為獨當一面的前端工程師。
-          </p>
+          {about.map((item, index) => (
+            <p
+              key={index}
+              className="py-2 leading-8 text-gray-800 dark:text-white"
+            >
+              <span className="block font-bold">【{item.title}】</span>
+              {item.content}
+            </p>
+          ))}
         </Section>
         <Section
           custom="dark:bg-slate-800 dark:rounded-md dark:border dark:border-slate-700"
           title="Bio"
         >
           {bioList.map((bio) => (
-            <WKX
+            <BioItem
               key={bio.id}
               title={bio.title}
               company={bio.company}
